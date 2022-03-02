98d104ff1a6f408b97e81669c181f1ef8b7de18c
nkshariharan <nkshariharan17@gmail.com>
2022-03-02 20:30:41 +0530

    adding routes folder

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,12 @@
 const express = require("express");
-
 const app = express();
+
+app.use(express.json())
+
+//route imports
+const products = require("./routes/productRoute");
+
+app.use("/api/v1", products)
 
 
 module.exports = app
