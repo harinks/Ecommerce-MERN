51c80e6ade427188a3fcdbb0102d7abe7dc98779
nkshariharan <nkshariharan17@gmail.com>
2022-03-03 13:17:18 +0530

    productCreate func

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,16 @@
+const productSchema = require("../models/productModel")
+
+//create Product - admin
+exports.createProduct = async(req,res,next)=>{
+
+    const product = await productSchema.create(req.body);
+    res.status(201).json({
+        success:true,
+        product
+    })
+}
+
+//get all products
 exports.getAllProducts = (req,res)=>{
     res.status(200).json({message:"sample route"})
 }
