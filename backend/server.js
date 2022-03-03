609de269637b1536a0c19bb12a0151f5dfe23214
nkshariharan <nkshariharan17@gmail.com>
2022-03-03 18:42:56 +0530

    handling uncaught and unhanled rejection error

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,29 @@ const app = require("./app");
 const connectDatabase = require("./config/db");
 
 
+// Handling Uncaught Exception
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Uncaught Exception`);
+  process.exit(1);
+});
+
+
 //config
-require("dotenv").config({ path:"backend/config/.env" })
+require("dotenv").config({ path: "backend/config/.env" })
 
 //connecting to db
 connectDatabase()
 
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server is working on http://localhost:${process.env.PORT}`);
+});
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is working on http://localhost:${process.env.PORT}`);
+// Unhandled Promise Rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Unhandled Promise Rejection`);
+  server.close(() => {
+    process.exit(1);
   });
+});
