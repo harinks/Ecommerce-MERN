f2381df34508b8d02d616b9e676d11d0b7829576
nkshariharan <nkshariharan17@gmail.com>
2022-03-04 15:03:54 +0530

    register route

--- backend/app.js
+++ backend/app.js
@@ -7,8 +7,10 @@ app.use(express.json())
 
 //route imports
 const products = require("./routes/productRoute");
+const users = require("./routes/userRoute")
 
 app.use("/api/v1", products)
+app.use("/api/v1", users)
 
 //middleware for errors
 app.use(errorMiddleware)
