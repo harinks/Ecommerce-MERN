ece6ad83ca62985694ba1825f26bbaef9f03cd72
nkshariharan <nkshariharan17@gmail.com>
2022-03-04 17:58:10 +0530

    product route and controller fix

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -4,79 +4,89 @@ const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const Apifeatures = require("../utils/apifeatures");
 
 //create Product - admin
-exports.createProduct = catchAsyncErrors(async(req,res,next)=>{
+exports.createProduct = catchAsyncErrors(async (req, res, next) => {
 
     const product = await productSchema.create(req.body);
     res.status(201).json({
-        success:true,
+        success: true,
         product
     });
 });
 
 //get all products
-exports.getAllProducts = catchAsyncErrors(async(req,res)=>{
+exports.getAllProducts = catchAsyncErrors(async (req, res) => {
 
     const resultPerPage = 5;
     const productCount = await productSchema.countDocuments();
 
-    const apifeatures = new Apifeatures(productSchema.find(),req.query)
-    .search()
-    .filter()
-    .pagination(resultPerPage);
+    const apifeatures = new Apifeatures(productSchema.find(), req.query)
+        .search()
+        .filter()
+        .pagination(resultPerPage);
 
     const products = await apifeatures.query;
     res.status(200).json({
-        success:true,
+        success: true,
         products,
         productCount
     });
 });
 
+// get all product (Admin)
+exports.getAdminProducts = catchAsyncErrors(async (req, res, next) => {
+    const products = await productSchema.find();
+
+    res.status(200).json({
+        success: true,
+        products,
+    });
+});
+
 //get single product
-exports.getProduct = catchAsyncErrors(async(req,res,next)=>{
+exports.getProduct = catchAsyncErrors(async (req, res, next) => {
     let product = await productSchema.findById(req.params.id);
 
-    if(!product){
+    if (!product) {
         return next(new ErrorHandler("Product not found", 404));
     }
     res.status(200).json({
-        success:true,
+        success: true,
         product
     });
 });
 
 //update product - admin
-exports.updateProduct = catchAsyncErrors(async(req,res,next)=>{
+exports.updateProduct = catchAsyncErrors(async (req, res, next) => {
     let product = await productSchema.findById(req.params.id);
 
-    if(!product){
+    if (!product) {
         return next(new ErrorHandler("Product not found", 404));
     }
 
-    product = await productSchema.findByIdAndUpdate(req.params.id,req.body,{
-        new:true,
-        runValidators:true,
-        useFindAndModify:false
+    product = await productSchema.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
     });
 
     res.status(200).json({
-        success:true,
+        success: true,
         product
     });
 });
 
 //delete product - admin
-exports.deleteProduct = catchAsyncErrors(async(req,res,next)=>{
+exports.deleteProduct = catchAsyncErrors(async (req, res, next) => {
     const product = await productSchema.findById(req.params.id);
 
-    if(!product){
+    if (!product) {
         return next(new ErrorHandler("Product not found", 404));
     }
 
     await product.remove();
 
     res.status(200).json({
-        success:true,
+        success: true,
         message: "product deleted"
     });
 });
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,10 +1,23 @@
 const express = require("express");
-const { getAllProducts, createProduct, updateProduct, deleteProduct, getProduct } = require("../controllers/productController");
 const router = express.Router();
+
+const { 
+    getAllProducts,
+    createProduct,
+    updateProduct,
+    deleteProduct,
+    getProduct, 
+    getAdminProducts
+} = require("../controllers/productController");
+
+const { isAuthenticatedUser } = require("../middleware/auth");
 
 
 router.route('/products').get(getAllProducts);
-router.route('/products/new').post(createProduct);
-router.route('/products/:id').put(updateProduct).delete(deleteProduct).get(getProduct);
+router.route('/product/:id').get(getProduct);
+
+router.route('/admin/products').get(isAuthenticatedUser, getAdminProducts);
+router.route('/admin/product/new').post(createProduct);
+router.route('/admin/product/:id').put(updateProduct).delete(deleteProduct);
 
 module.exports = router
