93af7ce09470f4363f3446f12a5e39af17fb1acb
nkshariharan <nkshariharan17@gmail.com>
2022-03-04 18:55:16 +0530

    authorizeroles middleware

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -10,14 +10,17 @@ const {
     getAdminProducts
 } = require("../controllers/productController");
 
-const { isAuthenticatedUser } = require("../middleware/auth");
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
 
 router.route('/products').get(getAllProducts);
 router.route('/product/:id').get(getProduct);
 
-router.route('/admin/products').get(isAuthenticatedUser, getAdminProducts);
-router.route('/admin/product/new').post(createProduct);
-router.route('/admin/product/:id').put(updateProduct).delete(deleteProduct);
+router.route('/admin/products').get(isAuthenticatedUser, authorizeRoles("admin"), getAdminProducts);
+router.route('/admin/product/new').post(isAuthenticatedUser, authorizeRoles("admin"), createProduct);
+router.route('/admin/product/:id')
+.get(isAuthenticatedUser, authorizeRoles("admin"), getProduct)
+.put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct)
+.delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct);
 
 module.exports = router
