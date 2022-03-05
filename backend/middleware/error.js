7971898b44979c79da2d3810f06a626e3a9f9857
nkshariharan <nkshariharan17@gmail.com>
2022-03-05 17:21:20 +0530

    error.js is fixed

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -11,6 +11,24 @@ module.exports = (err, req, res, next) => {
         err = new ErrorHandler(message, 400);
     }
 
+    // Mongoose duplicate key error
+    if (err.code === 11000) {
+        const message = `This ${Object.keys(err.keyValue)} is already exist`;
+        err = new ErrorHandler(message, 400);
+    }
+
+    // Wrong JWT error
+    if (err.name === "JsonWebTokenError") {
+        const message = `Token is invalid, Try again `;
+        err = new ErrorHandler(message, 400);
+    }
+
+    // JWT EXPIRE error
+    if (err.name === "TokenExpiredError") {
+        const message = `Token is Expired, Try again `;
+        err = new ErrorHandler(message, 400);
+    }
+
     res.status(err.statusCode).json({
         success: false,
         message: err.message,
