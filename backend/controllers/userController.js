171ae2ff7662490cfec5b877ec726d652063d73b
nkshariharan <nkshariharan17@gmail.com>
2022-03-05 17:51:58 +0530

    update user profile-user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -154,7 +154,7 @@ exports.updatePassword = catchAsyncErrors(async (req, res, next) => {
     }
 
     if (req.body.newPassword !== req.body.confirmPassword) {
-        return next(new ErrorHandler("password does not match", 400));
+        return next(new ErrorHandler("Confirm password does not match", 400));
     }
 
     user.password = req.body.newPassword;
@@ -164,3 +164,24 @@ exports.updatePassword = catchAsyncErrors(async (req, res, next) => {
     sendToken(user, 200, res);
 });
 
+// update User Profile
+exports.updateProfile = catchAsyncErrors(async (req, res, next) => {
+    const newUserData = {
+        name: req.body.name,
+        email: req.body.email,
+    };
+
+    //we need to update avatar photo - later
+
+    const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false,
+    });
+
+    res.status(200).json({
+        success: true,
+        user
+    });
+
+});
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -7,6 +7,7 @@ const {
     resetPassword,
     getUserDetails,
     updatePassword,
+    updateProfile,
 } = require("../controllers/userController");
 const { isAuthenticatedUser } = require("../middleware/auth");
 const router = express.Router();
@@ -20,6 +21,6 @@ router.route('/logout').get(logout);
 
 router.route("/me").get(isAuthenticatedUser, getUserDetails);
 router.route("/password/update").put(isAuthenticatedUser, updatePassword);
-
+router.route("/me/update").put(isAuthenticatedUser, updateProfile);
 
 module.exports = router;
