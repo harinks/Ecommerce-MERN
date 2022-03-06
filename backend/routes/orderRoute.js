5e2d1aa493507c91e18c4009d259d4b9749f5565
nkshariharan <nkshariharan17@gmail.com>
2022-03-06 17:56:55 +0530

    get all orders details - user

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -4,11 +4,13 @@ const router = express.Router();
 const { 
     newOrder, 
     getSingleOrder,
+    myOrders,
  } = require("../controllers/orderController");
 
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
 router.route("/order/new").post(isAuthenticatedUser, newOrder);
 router.route("/order/:id").get(isAuthenticatedUser, getSingleOrder);
+router.route("/orders/me").get(isAuthenticatedUser, myOrders);
 
 module.exports = router
