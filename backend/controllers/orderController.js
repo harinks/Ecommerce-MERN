a3db294fe6b98b3a7d67178d3cfbb0acbca88812
nkshariharan <nkshariharan17@gmail.com>
2022-03-06 18:42:18 +0530

    update status order - admin

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -76,3 +76,40 @@ exports.getAllOrders = catchAsyncErrors(async (req, res, next) => {
         orders,
     });
 });
+
+// update Order Status -- Admin
+exports.updateOrder = catchAsyncErrors(async (req, res, next) => {
+    const order = await Order.findById(req.params.id);
+
+    if (!order) {
+        return next(new ErrorHandler("Order not found with this Id", 404));
+    }
+
+    if (order.orderStatus === "Delivered") {
+        return next(new ErrorHandler("You have already delivered this order", 400));
+    }
+
+    if (req.body.status === "Shipped") {
+        order.orderItems.forEach(async (o) => {
+            await updateStock(o.product, o.quantity);
+        });
+    }
+    order.orderStatus = req.body.status;
+
+    if (req.body.status === "Delivered") {
+        order.deliveredAt = Date.now();
+    }
+
+    await order.save({ validateBeforeSave: false });
+    res.status(200).json({
+        success: true,
+    });
+});
+
+async function updateStock(id, quantity) {
+    const product = await productSchema.findById(id);
+
+    product.Stock -= quantity;
+
+    await product.save({ validateBeforeSave: false });
+}
--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -6,6 +6,7 @@ const {
     getSingleOrder,
     myOrders,
     getAllOrders,
+    updateOrder,
  } = require("../controllers/orderController");
 
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
@@ -15,5 +16,7 @@ router.route("/order/:id").get(isAuthenticatedUser, getSingleOrder);
 router.route("/orders/me").get(isAuthenticatedUser, myOrders);
 
 router.route("/admin/orders").get(isAuthenticatedUser, authorizeRoles("admin"), getAllOrders);
+router.route("/admin/order/:id")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateOrder);
 
 module.exports = router
