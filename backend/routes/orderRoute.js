2058541b0c0cc734ecbf0798aab2bead4df7196a
nkshariharan <nkshariharan17@gmail.com>
2022-03-06 18:00:19 +0530

    get all orders - admin

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -5,6 +5,7 @@ const {
     newOrder, 
     getSingleOrder,
     myOrders,
+    getAllOrders,
  } = require("../controllers/orderController");
 
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
@@ -12,5 +13,7 @@ const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 router.route("/order/new").post(isAuthenticatedUser, newOrder);
 router.route("/order/:id").get(isAuthenticatedUser, getSingleOrder);
 router.route("/orders/me").get(isAuthenticatedUser, myOrders);
+
+router.route("/admin/orders").get(isAuthenticatedUser, authorizeRoles("admin"), getAllOrders);
 
 module.exports = router
