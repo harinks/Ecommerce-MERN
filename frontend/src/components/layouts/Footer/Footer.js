6447361df3735686bb3cb8839efae708fb432b8c
nkshariharan <nkshariharan17@gmail.com>
2022-03-08 10:09:56 +0530

    footer component

---
+++ frontend/src/components/layouts/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Footer.css";
+
+function Footer() {
+    return (
+        <footer id="footer">
+            <div className="midFooter">
+                <h1>ECOMMERCE.</h1>
+                <p>Copyrights 2022 &copy; Hariharan nks</p>
+                <a href="https://github.com/harinks/Ecommerce-MERN" rel="noreferrer" target="_blank">Github</a>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
