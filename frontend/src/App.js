8622ceb3219a4dc4dcb4bd260ac805fca620e5f9
nkshariharan <nkshariharan17@gmail.com>
2022-03-08 10:14:48 +0530

    webfontloader

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,18 @@ import Header from './components/layouts/Header/Header.js';
 import { BrowserRouter as Router } from "react-router-dom";
 import Footer from './components/layouts/Footer/Footer';
 import WebFont from "webfontloader";
+import { useEffect } from 'react';
 
 function App() {
+  
+  useEffect(()=>{
+    WebFont.load({
+      google:{
+        families: ["Roboto", "Droid Sans", "Chilanka"]
+      }
+    })
+  },[]);
+
   return (
       <Router>
         <Header />
