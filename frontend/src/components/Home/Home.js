308c9dfad4f278d25a75e388e451355a2b806550
nkshariharan <nkshariharan17@gmail.com>
2022-03-09 19:26:20 +0530

    product.js

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,6 +1,15 @@
 import React, { Fragment } from 'react';
 import { CgMouse } from "react-icons/all";
 import './Home.css';
+import Product from './Product';
+
+const product = {
+    name: "Blue Shirt",
+    ratings: "4",
+    image: [{url:'https://img3.exportersindia.com/product_images/bc-full/2019/7/6447138/mens-shirts-1563255452-4999364.jpeg'}],
+    price: "500",
+    _id: "1234"
+}
 
 function Home() {
     return (
@@ -15,6 +24,17 @@ function Home() {
                         Scroll <CgMouse />
                     </button>
                 </a>
+            </div>
+
+            <h2 className="homeHeading">Featured Products</h2>
+            <div className="container" id="container">
+                <Product product={product} />
+                <Product product={product} />
+                <Product product={product} />
+                <Product product={product} />
+                <Product product={product} />
+                <Product product={product} />
+                <Product product={product} />
             </div>
         </Fragment>
     )
