705576923091051ea83c614752a718fc8a8c2727
nkshariharan <nkshariharan17@gmail.com>
2022-03-14 13:16:06 +0530

    search component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,8 @@ import WebFont from "webfontloader";
 import { useEffect } from 'react';
 import Home from './components/Home/Home';
 import ProductDetails from './components/Product/ProductDetails';
+import Products from './components/Product/Products';
+import Search from './components/Product/Search';
 
 function App() {
 
@@ -23,6 +25,8 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/product/:id' element={<ProductDetails />} />
+        <Route path='/products' element={<Products />} />
+        <Route path='/search' element={<Search />} />
       </Routes>
       <Footer />
     </Router>
